fa08aea79b51b9bb16d6c1cae6f9c34c5111ff37
root <root@root.com>
2017-11-03 19:33:11 +0700

    add ml.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 #!flask/bin/python
 from flask import Flask
+from ml import tes
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Hello, World!"
+
+@app.route('/tes')
+def coba():
+    return tes()
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ ml.py
@@ -0,0 +1,2 @@
+def tes():
+	return "halo"
